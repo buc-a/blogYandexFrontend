d3265de4780adc59e18ee00a241c1f9f5732736e
Angelina <butz.angelina@icloud.com>
2025-04-08 20:33:05 +0300

    fix: arrow button

--- src/components/arrow-button/ArrowButton.tsx
+++ src/components/arrow-button/ArrowButton.tsx
@@ -7,19 +7,27 @@ import styles from './ArrowButton.module.scss';
 /** Функция для обработки открытия/закрытия формы */
 export type OnClick = () => void;
 
-export const ArrowButton = () => {
+interface ArrowButtonProps {
+	state: boolean;
+	onClick: OnClick;
+}
+
+export const ArrowButton = ({state, onClick}: ArrowButtonProps) => {
 	return (
 		/* Не забываем указаывать role и aria-label атрибуты для интерактивных элементов */
 		<div
 			role='button'
 			aria-label='Открыть/Закрыть форму параметров статьи'
 			tabIndex={0}
-			className={styles.container}
+			//добавляем класс в зависимости от состояния
+			className={clsx(styles.container, state ? styles.container_open : null)}
+			onClick={onClick}
 		>
 			<img
 				src={arrow}
 				alt='иконка стрелочки'
-				className={styles.arrow}
+				//добавляем класс в зависимости от состояния
+				className={clsx(styles.arrow, state ? styles.arrow_open : null)}
 			/>
 		</div>
 	);
